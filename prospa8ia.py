0ab345aef00364e797a1b010852a2eec0490a581
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2018-10-18 11:08:24 +0200

    Update prospa8ia.py

--- prospa8ia.py
+++ prospa8ia.py
@@ -3,13 +3,14 @@
 Created on Sun Dec  3 15:53:41 2017
 
 @author: ANASTASIS
+
 """
 
 # Importing the libraries
 import numpy as np
 import pandas as pd
 
-# Importing the dataset
+# Importing the datasets
 dataset = pd.read_json('TESTING_Basmati-Track_2575761290148.json', lines=True)
 geofence = pd.read_json('Geofences.json')
 X = dataset.iloc[:, [2, 3]].values
